44dacc13524d3dc3477944678c2f84e13401c5b2
Ryan-Fixes-Stuff <ryanbarton.solutions@gmail.com>
2024-11-15 13:57:30 -0600

    Testing
    Added Tasks to XUnit testing project, task created based on the test Vehicle's VIN.

--- MaintenanceTracker.Tests/ModelTester.cs
+++ MaintenanceTracker.Tests/ModelTester.cs
@@ -14,31 +14,64 @@ namespace MaintenanceTracker.Tests
         public async void CRUDTest()
         {
             // launch a create, read, update, and delete sequentially after generating some Vehicle properties
+            // mix in Task CRUD based on test vehicle VIN
+
+            // SETUP
             var config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", false, true)
             .Build();
             Vehicle validVehicle = new();
-            IDataModel dm = new DataModel(config);
+            DataModel dm = new(config);
             validVehicle.VIN = "A9T28A798TA98T79A";
             validVehicle.Make = "Chrysler";
             validVehicle.Model = "Voyager";
             validVehicle.Year = 2009;
             validVehicle.Price = 8500.00;
-            //create
+            // END SETUP
+
+            // START TESTS
+            //create vehicle
             int createResult = await dm.CreateVehicle(validVehicle);
             Assert.Equal(1, createResult);
-            //read
+            //read vehicles
             List<Vehicle> vehicles = dm.ReadVehicles();
             Assert.NotNull(vehicles);
-            Assert.True(vehicles.Any());
-            //update
+            Assert.True(vehicles.Count != 0);
+            //update vehicle
             validVehicle.Year = 2011;
-            int updateResult = await dm.UpdateVehicle(validVehicle, validVehicle.VIN);
-            Assert.Equal(1, updateResult); 
-            //delete
+            // add task to vehicle
+            MaintenanceTask task = new();
+            task.VIN = validVehicle.VIN;
+            task.TaskName = "Oil Change";
+            task.Status = "New";
+            int createTaskResult = await dm.CreateTask(task);
+            Assert.Equal(1, createTaskResult);
+            // update task
+            task.Status = "Accepted";
+            int updateTaskResult = await dm.UpdateTask(task, validVehicle.VIN, task.TaskName);
+            Assert.Equal(1, updateTaskResult);
+            //update vehicle then update tasks
+            string oldVIN = validVehicle.VIN;
+            validVehicle.VIN = "A9T28A798TA98T79B";
+            int updateResult = await dm.UpdateVehicle(validVehicle, task.VIN);
+            Assert.Equal(1, updateResult);
+            // update Tasks accordingly
+            List<MaintenanceTask> tasks = dm.ReadTasks(task.VIN);
+            int taskVINUpdateResult = 0;
+            foreach (MaintenanceTask t in tasks){
+                t.VIN = validVehicle.VIN;
+                taskVINUpdateResult += await dm.UpdateTask(t, oldVIN, t.TaskName);
+            }
+            task.VIN = validVehicle.VIN;
+            Assert.Equal(1, taskVINUpdateResult);
+            // delete Task
+            int deleteTaskResult = await dm.DeleteTask(task);
+            Assert.Equal(1, deleteTaskResult);
+            // delete Vehicle
             int deleteResult = await dm.DeleteVehicle(validVehicle);
             Assert.Equal(1, deleteResult);
+            // FINISH TESTS
         }
     }
 }
